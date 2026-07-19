b10e4a35bbf7934d51cde3601ad9ce8f60ced74d
nattathalia <nattathalia630@gmail.com>
2026-07-19 19:46:18 +0700

    Update iframe URLs for video entries

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const videos = [
   {
     title: "Xvideos shake it first before putting it in SD",
-    iframe: "https://luluvdo.com/embed/6mtdn8yfookj",
+    iframe: "https://luluvdo.com/e/6mtdn8yfookj",
   },
   {
     title: "Xvideos hijab teen has a naughty proposal for her coach SD",
-    iframe: "https://luluvdo.com/embed/ejo2juvwelha",
+    iframe: "https://luluvdo.com/e/ejo2juvwelha",
   },
   {
     title: "Video 3",
