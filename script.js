c72b6e032598286848bb532b57a525c4d2d89c98
nattathalia <nattathalia630@gmail.com>
2026-07-19 19:49:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const videos = [
   {
-    title: "Xvideos shake it first before putting it in SD",
+    title: "Xvideos shake it first before putting it in",
     iframe: "https://luluvdo.com/e/6mtdn8yfookj",
   },
   {
-    title: "Xvideos hijab teen has a naughty proposal for her coach SD",
+    title: "Xvideos hijab teen has a naughty proposal for her coach",
     iframe: "https://luluvdo.com/e/ejo2juvwelha",
   },
   {
