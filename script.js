c44cb7d8e382515fd0423d7ef67c811f32ccae7c
nattathalia <nattathalia630@gmail.com>
2026-07-19 21:49:04 +0700

    Change video entry to iframe format
    Updated video entry to use iframe format for embedding.

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 const videos = [
-  {
-    title: "Xvideos shake it first before putting it in",
-    iframe: "https://luluvdo.com/e/6mtdn8yfookj",
-  },
-  {
+  
+  <iframe
+  src:"https://luluvdo.com/e/6mtdn8yfookj"
+  title:"Xvideos shake it first before putting it in SD"
+  scrolling="no" 
+  frameborder="0" 
+  width="640" 
+  height="360" 
+  allowfullscreen="true" 
+  webkitallowfullscreen="true" 
+  mozallowfullscreen="true"></iframe>
+  
+  
     title: "Xvideos hijab teen has a naughty proposal for her coach",
     iframe: "https://luluvdo.com/e/ejo2juvwelha",
   },
