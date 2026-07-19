8921ded79cf3c885f7147d91d2b38202e632cc6c
nattathalia <nattathalia630@gmail.com>
2026-07-19 17:57:23 +0700

    Update video embed link in script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const videos = [
 
 {
 title:"Bu Guru Jilbab Syumilde Kena Doggy",
-embed:"https://luluvdo.com/6mtdn8yfookj"
+<iframe src="https://luluvdo.com/e/x7pgxi7ejra8" scrolling="no" frameborder="0" width="640" height="360" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"></iframe>
 },
   
 {
