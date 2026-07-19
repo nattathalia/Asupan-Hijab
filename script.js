9666007b488aed0c52ae9e67b20fda5686d028a0
nattathalia <nattathalia630@gmail.com>
2026-07-19 22:17:43 +0700

    Clean up video entries in script.js
    Removed multiple video entries from the videos array.

--- script.js
+++ script.js
@@ -1,131 +1,130 @@
 const videos = [ 
+  
 {
   title:"Xvideos shake it first before putting it in SD",
   embed:"https://luluvdo.com/e/6mtdn8yfookj",
 },
-
-
-    title: "Xvideos hijab teen has a naughty proposal for her coach",
-    iframe: "https://luluvdo.com/e/ejo2juvwelha",
-  },
-  {
-    title: "Xvideos hijab teen alexia anders is excited to finally have her",
-    iframe: "https://luluvdo.com/e/1vbeuvw00xrg",
-  },
-  {
-    title: "Xvideos arab sex hijab stepsister saudi fucks deliciously",
-    iframe: "https://luluvdo.com/e/yzy5prwrosin",
-  },
-  {
-    title: "Anal hijab tobrut pertama kali kesakitan",
-    iframe: "https://luluvdo.com/e/t5no683qlkiq",
-  },
-  {
-    title: "G0yangan Pacar Hijab Cantik Mmxnya Mntap",
-    iframe: "https://luluvdo.com/e/d33s8ouspsf0",
-  },
-  {
-    title: "tanye hijab pap colmek ke selingkuhannya",
-    iframe: "https://luluvdo.com/e/l0rqexugyrji",
-  },
-  {
-    title: "Hijab umay bikin konten ekslusif",
-    iframe: "https://luluvdo.com/e/23b3j0hae8d9",
-  },
-  {
-    title: "Bokep indo - Hijab Cantik Sange Malu Malu",
-    iframe: "https://luluvdo.com/e/3n94zibmjvqu",
-  },
-  {
-    title: "Abg Hijab Baru Pertama Kali Nyobain",
-    iframe: "https://luluvdo.com/e/gwdpb400irja",
-  },
-  {
-    title: "133737 mp4",
-    iframe: "https://luluvdo.com/e/5p02q9p55jzw",
-  },
-  {
-    title: "Bokep Indo Hijab Kacamata SMA Batik Colmek",
-    iframe: "https://luluvdo.com/e/72dazlfqaoyz",
-  },
-  {
-    title: "Sex ABG Jilbab Indonesia Masturbasi",
-    iframe: "https://luluvdo.com/e/y6mfgjkhzxsk",
-  },
-  {
-    title: "Ngentot Cewe Jilbab SMA",
-    iframe: "https://luluvdo.com/e/86yk4l0mhvhd",
-  },
-  {
-    title: "Jilbab Tocil Memek Mulus",
-    iframe: "https://luluvdo.com/e/0mamr14ilt2y",
-  },
-  {
-    title: "Jilbab Tobrut Ngentot Sampe Becek",
-    iframe: "https://luluvdo.com/e/4y5scyalmv1z",
-  },
-  {
-    title: "Jilbab Semok",
-    iframe: "https://luluvdo.com/e/w3rw7z1f4zxl",
-  },
-  {
-    title: "Jilbab SMA Ngewe Bikin Konten",
-    iframe: "https://luluvdo.com/e/25skfdiklmfs",
-  },
-  {
-    title: "Jilbab Kepengen",
-    iframe: "https://luluvdo.com/e/ly7z4k9huspg",
-  },
-  {
-    title: "Ceweknya Cakep",
-    iframe: "https://luluvdo.com/e/pbol01fxw3uf",
-  },
-  {
-    title: "ABG Jilbab Masturbasi",
-    iframe: "https://luluvdo.com/e/d32l7mzcvqcg",
-  },
-  {
-    title: "ABG Hijab Cakep Buka Jilbab Benerin Tetek",
-    iframe: "https://luluvdo.com/e/jurphqq5sq5k",
-  },
-  {
-    title: "Skandal Hijab Mirip Influencer Terkenal",
-    iframe: "https://luluvdo.com/e/gxcsshn1yvpv",
-  },
-  {
-    title: "Pacar Hijab Menikmati Banget",
-    iframe: "https://luluvdo.com/e/7saveth4cxrm",
-  },
-  {
-    title: "Mahasiswi Jilbab Sange dari Bandung",
-    iframe: "https://luluvdo.com/e/x4li42iqpnfz",
-  },
-  {
-    title: "Mahasiswi Jilbab Colmek Di Toilet Kampus",
-    iframe: "https://luluvdo.com/e/hrryhgl22c82",
-  },
-  {
-    title: "Hijabers Della Ajarin Pacar Belum Pernah Ngewe",
-    iframe: "https://luluvdo.com/e/ezu4kewi1ygy",
-  },
-  {
-    title: "Hijaber Eks Pacar Katanya Belum Pernah Dipegang Lagi",
-    iframe: "https://luluvdo.com/e/hrryhgl22c82",
-  },
-  {
-    title: "Hijab Kebaya Ungu Meki Gundul",
-    iframe: "https://luluvdo.com/e/pa5ky1y6qgtt",
-  },
-  {
-    title: "Gen-Z Main Diparkiran Malah Tambah Nafsu",
-    iframe: "https://example.com/e/video30",
-  },
-  {
-    title: "Enak Kalo Punya Pacar Tante Hijab Tiap Hari Dimanjain",
-    iframe: "https://luluvdo.com/e/lcghvu7n6v2e",
-  },
-  {
-    title: "Bu Guru Jilbab Syumilde Kena Doggy",
-    iframe: "https://luluvdo.com/e/x7pgxi7ejra8",
-  }
+{
+  title: "Xvideos hijab teen has a naughty proposal for her coach",
+  embed: "https://luluvdo.com/e/ejo2juvwelha",
+},
+{
+  title: "Xvideos hijab teen alexia anders is excited to finally have her",
+  embed: "https://luluvdo.com/e/1vbeuvw00xrg",
+},
+{
+  title: "Xvideos arab sex hijab stepsister saudi fucks deliciously",
+  embed: "https://luluvdo.com/e/yzy5prwrosin",
+},
+{
+  title: "Anal hijab tobrut pertama kali kesakitan",
+  embed: "https://luluvdo.com/e/t5no683qlkiq",
+},
+{
+  title: "G0yangan Pacar Hijab Cantik Mmxnya Mntap",
+  embed: "https://luluvdo.com/e/d33s8ouspsf0",
+},
+{
+  title: "tanye hijab pap colmek ke selingkuhannya",
+  embed: "https://luluvdo.com/e/l0rqexugyrji",
+},
+{
+  title: "Hijab umay bikin konten ekslusif",
+  embed: "https://luluvdo.com/e/23b3j0hae8d9",
+},
+{
+  title: "Bokep indo - Hijab Cantik Sange Malu Malu",
+  embed: "https://luluvdo.com/e/3n94zibmjvqu",
+},
+{
+  title: "Abg Hijab Baru Pertama Kali Nyobain",
+  embed: "https://luluvdo.com/e/gwdpb400irja",
+},
+{
+  title: "133737 mp4",
+  embed: "https://luluvdo.com/e/5p02q9p55jzw",
+},
+{
+  title: "Bokep Indo Hijab Kacamata SMA Batik Colmek",
+  embed: "https://luluvdo.com/e/72dazlfqaoyz",
+},
+{
+  title: "Sex ABG Jilbab Indonesia Masturbasi",
+  embed: "https://luluvdo.com/e/y6mfgjkhzxsk",
+},
+{
+  title: "Ngentot Cewe Jilbab SMA",
+  embed: "https://luluvdo.com/e/86yk4l0mhvhd",
+},
+{
+  title: "Jilbab Tocil Memek Mulus",
+  embed: "https://luluvdo.com/e/0mamr14ilt2y",
+},
+{
+  title: "Jilbab Tobrut Ngentot Sampe Becek",
+  embed: "https://luluvdo.com/e/4y5scyalmv1z",
+},
+{
+  title: "Jilbab Semok",
+  embed: "https://luluvdo.com/e/w3rw7z1f4zxl",
+},
+{
+  title: "Jilbab SMA Ngewe Bikin Konten",
+  embed: "https://luluvdo.com/e/25skfdiklmfs",
+},
+{
+  title: "Jilbab Kepengen",
+  embed: "https://luluvdo.com/e/ly7z4k9huspg",
+},
+{
+  title: "Ceweknya Cakep",
+  embed: "https://luluvdo.com/e/pbol01fxw3uf",
+},
+{
+  title: "ABG Jilbab Masturbasi",
+  embed: "https://luluvdo.com/e/d32l7mzcvqcg",
+},
+{
+  title: "ABG Hijab Cakep Buka Jilbab Benerin Tetek",
+  embed: "https://luluvdo.com/e/jurphqq5sq5k",
+},
+{
+  title: "Skandal Hijab Mirip Influencer Terkenal",
+  embed: "https://luluvdo.com/e/gxcsshn1yvpv",
+},
+{
+  title: "Pacar Hijab Menikmati Banget",
+  embed: "https://luluvdo.com/e/7saveth4cxrm",
+},
+{
+  title: "Mahasiswi Jilbab Sange dari Bandung",
+  embed: "https://luluvdo.com/e/x4li42iqpnfz",
+},
+{
+  title: "Mahasiswi Jilbab Colmek Di Toilet Kampus",
+  embed: "https://luluvdo.com/e/hrryhgl22c82", },
+{
+  title: "Hijabers Della Ajarin Pacar Belum Pernah Ngewe",
+  embed: "https://luluvdo.com/e/ezu4kewi1ygy",
+},
+{
+  title: "Hijaber Eks Pacar Katanya Belum Pernah Dipegang Lagi",
+  embed: "https://luluvdo.com/e/hrryhgl22c82",
+},
+{
+  title: "Hijab Kebaya Ungu Meki Gundul",
+  embed: "https://luluvdo.com/e/pa5ky1y6qgtt",
+},
+{
+  title: "Gen-Z Main Diparkiran Malah Tambah Nafsu",
+  embed: "https://example.com/e/video30",
+},
+{
+  title: "Enak Kalo Punya Pacar Tante Hijab Tiap Hari Dimanjain",
+  embed: "https://luluvdo.com/e/lcghvu7n6v2e",
+},
+{
+  title: "Bu Guru Jilbab Syumilde Kena Doggy",
+  embed: "https://luluvdo.com/e/x7pgxi7ejra8",
+}
 ];
