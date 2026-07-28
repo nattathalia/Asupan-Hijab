5e91bdf2ab9d0f05ce9ee93f6939cdc7d7448d44
nattathalia <nattathalia630@gmail.com>
2026-07-28 11:27:01 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,40 @@
+const videos = {
+  home: [
+    {
+      title: "Video Beranda 1",
+      embed: "https://www.youtube.com/embed/VIDEO_ID_1"
+    },
+    {
+      title: "Video Beranda 2",
+      embed: "https://www.youtube.com/embed/VIDEO_ID_2"
+    }
+  ],
+
+  sekolah: [
+    {
+      title: "Video Sekolah 1",
+      embed: "https://www.youtube.com/embed/VIDEO_ID_3"
+    }
+  ],
+
+  remaja: [
+    {
+      title: "Video Remaja 1",
+      embed: "https://www.youtube.com/embed/VIDEO_ID_4"
+    }
+  ],
+
+  favorit: [
+    {
+      title: "Video Favorit 1",
+      embed: "https://www.youtube.com/embed/VIDEO_ID_5"
+    }
+  ],
+
+  bule: [
+    {
+      title: "Video Bule 1",
+      embed: "https://www.youtube.com/embed/VIDEO_ID_6"
+    }
+  ]
+};
